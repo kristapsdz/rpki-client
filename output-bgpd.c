250bf42a2bf5f0cdf88519833d16b5a55fcfaf18
kristaps <>
2019-11-17 14:35:49 +0000

    Use an RB tree to track VRPs.  From OpenBSD.  This is using an experimental oconfigure build that will be versioned after testing.

--- output-bgpd.c
+++ output-bgpd.c
@@ -16,67 +16,28 @@
  */
 #include "config.h"
 
-#include <assert.h>
-#include <err.h>
-#include <inttypes.h>
-#include <stdarg.h>
 #include <stdlib.h>
-
 #include <openssl/ssl.h>
 
 #include "extern.h"
 
-static int
-cmp(const void *p1, const void *p2)
-{
-	const char *a1 = *(const char **)p1, *a2 = *(const char **)p2;
-
-	return strcmp(a1, a2);
-}
-
 void
-output_bgpd(FILE *out, const struct roa **roas, size_t roasz,
-    size_t *vrps, size_t *unique)
+output_bgpd(FILE *out, struct vrp_tree *vrps)
 {
 	char		 buf1[64], buf2[32];
-	char		**lines = NULL;
-	size_t		 i, j, k;
-
-	*vrps = *unique = 0;
-
-	for (i = 0; i < roasz; i++)
-		*vrps += roas[i]->ipsz;
-
-	if ((lines = calloc(*vrps, sizeof(char *))) == NULL)
-		err(EXIT_FAILURE, NULL);
-
-	for (i = k = 0; i < roasz; i++)
-		for (j = 0; j < roas[i]->ipsz; j++) {
-			ip_addr_print(&roas[i]->ips[j].addr,
-			    roas[i]->ips[j].afi, buf1, sizeof(buf1));
-			if (roas[i]->ips[j].maxlength >
-			    roas[i]->ips[j].addr.prefixlen)
-				snprintf(buf2, sizeof(buf2), "maxlen %zu ",
-				    roas[i]->ips[j].maxlength);
-			else
-				buf2[0] = '\0';
-			if (asprintf(&lines[k++], "%s %ssource-as %u",
-			    buf1, buf2, roas[i]->asid) == -1)
-				err(EXIT_FAILURE, NULL);
-		}
-
-	assert(k == *vrps);
-	qsort(lines, *vrps, sizeof(char *), cmp);
+	struct vrp	*v;
 
 	fprintf(out, "roa-set {\n");
-	for (i = 0; i < *vrps; i++)
-		if (i == 0 || strcmp(lines[i], lines[i - 1])) {
-			fprintf(out, "\t%s\n", lines[i]);
-			(*unique)++;
-		}
-	fprintf(out, "}\n");
 
-	for (i = 0; i < *vrps; i++)
-		free(lines[i]);
-	free(lines);
+	RB_FOREACH(v, vrp_tree, vrps) {
+		ip_addr_print(&v->addr, v->afi, buf1, sizeof(buf1));
+		if (v->maxlength > v->addr.prefixlen)
+			snprintf(buf2, sizeof(buf2), "maxlen %u ",
+			    v->maxlength);
+		else
+			buf2[0] = '\0';
+		fprintf(out, "\t%s %ssource-as %u\n", buf1, buf2, v->asid);
+	}
+
+	fprintf(out, "}\n");
 }
